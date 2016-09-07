3bc84ca4d0699f27247f052b4f1a4d97764ebfd1
Daved <daved@codemodus.com>
2016-09-06 17:52:10 -0700

    Add players and player routing.

---
+++ starting/src-elm/Routing.elm
@@ -0,0 +1,43 @@
+module Routing exposing (..)
+
+import String
+import Navigation
+import UrlParser exposing (..)
+import Players.Models exposing (PlayerID)
+
+
+type Route
+    = PlayersRoute
+    | PlayerRoute PlayerID
+    | NotFoundRoute
+
+
+matchers : Parser (Route -> a) a
+matchers =
+    oneOf
+        [ format PlayersRoute (s "")
+        , format PlayerRoute (s "players" </> int)
+        , format PlayersRoute (s "players")
+        ]
+
+
+hashParser : Navigation.Location -> Result String Route
+hashParser location =
+    location.hash
+        |> String.dropLeft 1
+        |> parse identity matchers
+
+
+parser : Navigation.Parser (Result String Route)
+parser =
+    Navigation.makeParser hashParser
+
+
+routeFromResult : Result String Route -> Route
+routeFromResult result =
+    case result of
+        Ok route ->
+            route
+
+        Err string ->
+            NotFoundRoute
